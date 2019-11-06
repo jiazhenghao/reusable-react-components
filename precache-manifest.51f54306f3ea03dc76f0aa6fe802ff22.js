38071d06d79da4f7bdf73ab21e4cd97392ff86d2
Zhenghao Jia <cherie-xia@hotmail.com>
2019-11-06 19:45:40 +0800

    Updates

---
+++ precache-manifest.51f54306f3ea03dc76f0aa6fe802ff22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54df5dc8c31f80ea86c4c93552f79557",
+    "url": "/index.html"
+  },
+  {
+    "revision": "518d9322ed0bdad0bf75",
+    "url": "/static/css/2.52738377.chunk.css"
+  },
+  {
+    "revision": "f298ad576cabc7f46f89",
+    "url": "/static/css/main.b7d2544c.chunk.css"
+  },
+  {
+    "revision": "518d9322ed0bdad0bf75",
+    "url": "/static/js/2.7e2f4ce4.chunk.js"
+  },
+  {
+    "revision": "f298ad576cabc7f46f89",
+    "url": "/static/js/main.f07e1f97.chunk.js"
+  },
+  {
+    "revision": "4f84186483e47ff083ec",
+    "url": "/static/js/runtime-main.4fb116d7.js"
+  }
+]);
